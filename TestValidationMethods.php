d74f1f86ecc674d10e2f81924fa1aae82bc1bec5
KeonaG <keonag@ubuntu20.04.4.myguest.virtualbox.org>
2024-03-08 11:35:51 -0800

    Adding url and name validation

--- TestValidationMethods.php
+++ TestValidationMethods.php
@@ -11,10 +11,34 @@ class ValidationMethods{
         }
     }
     
+    public function validateName($name){
+        if(ctype_alpha(str_replace(' ', '', $name)) === false){
+            $ErrMsg = "Name must contain letters and spaces only";
+            echo $ErrMsg;
+            
+            return "Invalid";
+        }else{
+            echo ":" . $name . ":";
+            return "Valid";
+        }
+    }
+    
+    public function validateURL($url){
+        if(filter_var($url, FILTER_VALIDATE_URL) === false){
+            $ErrMsg = "Not a valid URL";
+            echo $ErrMsg;
+            return "Invalid";
+        }else{
+            return "Valid";
+        }
+    }
+    
 }
 
 class TestValidationMethods extends \PHPUnit\Framework\TestCase{
-        
+        /*
+         * Email field validation tests
+         */
         public function testEmailValidation(){
                 $tester = new ValidationMethods();
                 $result = $tester->validateEmail("kohorstkeona@outlook.com");
@@ -29,8 +53,75 @@ class TestValidationMethods extends \PHPUnit\Framework\TestCase{
         
         public function testEmailValidation3(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateEmail("kohorstkeona$%");
+                $result = $tester->validateEmail("kohorst#keona$%");
                 $this->assertEquals("Invalid", $result);
+        }
+        
+        
+        /*
+         * Name field validation tests
+         */
+        public function testNameValidation(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateName("Keona");
+                $this->assertEquals("Valid", $result);
+        }
+        
+        public function testNameValidation2(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateName("$%^&*(");
+                $this->assertEquals("Invalid", $result);
+        }
+        
+        public function testNameValidation3(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateName("Keona                 ");
+                $this->assertEquals("Valid", $result);
+        }
+        
+        public function testNameValidation4(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateName("Elizabeth Joy");
+                $this->assertEquals("Valid", $result);
+        }
+        
+        public function testNameValidation5(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateName("Susan-Anne");
+                $this->assertEquals("Invalid", $result);
+        }
+        
+        /*
+         * URL field validation tests
+         */
+        public function testURLValidation(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateURL("https://www.google.com");
+                $this->assertEquals("Valid", $result);
+        }
+        
+        public function testURLValidation2(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateURL("google.com");
+                $this->assertEquals("Invalid", $result);
+        }
+        
+        public function testURLValidation3(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateURL("yahoo");
+                $this->assertEquals("Invalid", $result);
+        }
+        
+        public function testURLValidation4(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateURL("www.google.com");
+                $this->assertEquals("Invalid", $result);
+        }
+        
+        public function testURLValidation5(){
+                $tester = new ValidationMethods();
+                $result = $tester->validateURL("https://forums.phpfreaks.com/topic/284903-allow-only-letters-numbers-dash-underscore-or-space/");
+                $this->assertEquals("Valid", $result);
         }
 }
 
