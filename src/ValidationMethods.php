b35575e383c8f43e1369b2218077d72d80bd06d2
KeonaG <keonag@ubuntu20.04.4.myguest.virtualbox.org>
2024-03-23 11:21:11 -0700

    First commit

--- src/ValidationMethods.php
+++ src/ValidationMethods.php
@@ -3,64 +3,155 @@
 namespace Keonag\UnitTests;
 class ValidationMethods{
     
-    public function validateEmail($email){
+    /**
+     * Returns true if email is valid format, false if not.
+     * 
+     * @param string $email The email to be validated
+     * @return bool
+     */
+    public function validate_email($email){
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Email is a valid format\n";
-            return "Valid";
+            $result_message = "Email " . $email . " is a valid format\n";
+            echo $result_message;
+            return true;
         } else {
-            echo "Email is an invalid format\n";
-            return "Invalid";
+            $result_message = "Email " . $email . " is an invalid format\n";
+            echo $result_message;
+            return false;
         }
     }
     
-    public function validateName($name){
+    /**
+     * Checks if the given name consists of only letters.
+     * Returns true if only letters, false if not.
+     * 
+     * @param string $name The name to be validated
+     * @return bool
+     */
+    public function validate_name($name){
         //sanitize spaces
+        //$name = sanitize_string($name);
         $name = trim($name);
         $name = preg_replace('/\s\s+/', '', $name);
         $name = preg_replace('/\t+/', '', $name);
         $name = preg_replace('/\n\r+/', '', $name);
-        $name = ucwords(strtolower($name));
+        $name = ucwords(strtolower($name)); //put in title case
         
         //check for non alphabetic characters
-        if(ctype_alpha(str_replace(' ', '', $name)) === false){
-            $ErrMsg = "Name must contain letters and spaces only\n";
-            echo $ErrMsg;
+        if(!$this->only_letters($name)){
+            $result_message = "Name " . $name . " must contain letters and spaces only\n";
+            echo $result_message;
             
-            return "Invalid";
+            return false;
         }else{
-            echo ":" . $name . ":\n";
-            return "Valid";
+            $result_message = "Name " . $name . " is valid\n";
+            echo $result_message;
+            return true;
         }
     }
     
-    public function validateURL($url){
+    /**
+     * Checks if the given URL is in the correct format
+     * 
+     * @param string $url The URL to be validated
+     * @return bool
+     */
+    public function validate_URL($url){
         if(filter_var($url, FILTER_VALIDATE_URL) === false){
-            $ErrMsg = "Not a valid URL\n";
-            echo $ErrMsg;
-            return "Invalid";
+            $result_message = $url . " is not a valid URL\n";
+            echo $result_message;
+            return false;
         }else{
-            return "Valid";
+            $result_message = $url . " is a valid URL\n";
+            echo $result_message;
+            return true;
         }
     }
     
-    public function validateTitle($title){
+    /**
+     * Checks if the title contains at least one letter
+     * Returns true if yes, returns false if no.
+     * 
+     * Also sanitizes the input string.
+     * 
+     * @param string $title The title to be validated
+     * @return bool
+     */
+    public function validate_title($title){
+        //$title = this->sanitize_string($title);
         $title = trim($title);
         $title = preg_replace('/\s\s+/', '', $title);
         $title = preg_replace('/\t+/', '', $title);
         $title = preg_replace('/\n\r+/', '', $title);
-        $title = ucwords(strtolower($title));
+        $title = ucwords(strtolower($title)); //put in title case
         
-        //check to make sure it contains some letters
-        $contains_letters = preg_match('/\pL/', $title);
-        
-        if(!$contains_letters){
-            $ErrMsg = "Title must contain alphabetic characters\n";
-            echo $ErrMsg;
-            return "Invalid";
+        if(!$this->contains_letters($title)){
+            $result_message = "Title " . $title . " must contain alphabetic characters\n";
+            echo $result_message;
+            return false;
         }else{
-            echo $title . "\n";
-            return "Valid";
+            $result_message = "Title " . $title . " is valid\n";
+            echo $result_message;
+            return true;
         }
     }
     
+    /**
+     * Checks if the description has at least 1 letter.
+     * Returns false if it doesn't, returns true if it does.
+     * 
+     * @param string $description The description to validate
+     * @return bool
+     */
+    public function validate_description($description){
+        if(!$this->contains_letters($description)){
+            $result_message = "Description " . $description . " must contain alphabetic characters\n";
+            echo $result_message;
+            return false;
+        }else{
+            $result_message = "Description " . $description . " is valid\n";
+            echo $result_message;
+            return true;
+        }
+    }
+    
+   /**
+    * Removes unnecessary white space from a string and puts it in title case
+    * and returns the sanitized string
+    * 
+    * @param string $string The string to sanitize
+    */
+   public function sanitize_string($string){
+       echo "String to sanitize" . $string . " : ";
+       $string = trim($string);
+       $string = preg_replace('/\s\s+/', ' ', $string);
+       $string = preg_replace('/\t+/', '', $string);
+       $string = preg_replace('/\n\r+/', '', $string);
+       $string = ucwords(strtolower($string)); //put in title case
+       
+       echo "Sanitized: " . $string . "\n";
+
+       return $string;
+   }
+   
+   /**
+    * Checks if the given value contains at least 1 letter
+    * 
+    * @param string $value The string to check if it contains letters
+    */
+   public function contains_letters($value){
+       $contains_letters = preg_match('/\pL/', $value);
+       return $contains_letters;
+   }
+   
+   /**
+    * Checks if the given string is made up of only alphabetic characters
+    * 
+    * @param string $value The string to check
+    */
+   public function only_letters($value) {
+       $alphabetOnly = ctype_alpha(str_replace(' ', '', $value));
+       return $alphabetOnly;
+   }
+
 }
--- tests/TestValidationMethods.php
+++ tests/TestValidationMethods.php
@@ -2,125 +2,217 @@
 
 namespace Keonag\UnitTests;
 
+require_once '/home/keonag/unit-tests/src/ValidationMethods.php';
+
 class TestValidationMethods extends \PHPUnit\Framework\TestCase{
         /*
          * Email field validation tests
          */
-        public function testEmailValidation(){
+        public function test_email_validation(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateEmail("kohorstkeona@outlook.com");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_email("kohorstkeona@outlook.com");
+                $this->assertEquals(true, $result);
         }
         
-        public function testEmailValidation2(){
+        public function test_email_validation_2(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateEmail("kohorstkeona@outlook");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_email("kohorstkeona@outlook");
+                $this->assertEquals(false, $result);
         }
         
-        public function testEmailValidation3(){
+        public function test_email_validation_3(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateEmail("kohorst#keona$%");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_email("kohorst#keona$%");
+                $this->assertEquals(false, $result);
         }
         
         
         /*
          * Name field validation tests
          */
-        public function testNameValidation(){
+        public function test_name_validation(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateName("Keona");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_name("Keona");
+                $this->assertEquals(true, $result);
         }
         
-        public function testNameValidation2(){
+        public function test_name_validation_2(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateName("$%^&*(");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_name("$%^&*(");
+                $this->assertEquals(false, $result);
         }
         
-        public function testNameValidation3(){
+        public function test_name_validation_3(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateName("Keona                 ");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_name("Keona                 ");
+                $this->assertEquals(true, $result);
         }
         
-        public function testNameValidation4(){
+        public function test_name_validation_4(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateName("Elizabeth Joy");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_name("Elizabeth Joy\n");
+                $this->assertEquals(true, $result);
         }
         
-        public function testNameValidation5(){
+        public function test_name_validation_5(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateName("Susan-Anne");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_name("Susan-Anne");
+                $this->assertEquals(false, $result);
         }
         
         /*
          * URL field validation tests
          */
-        public function testURLValidation(){
+        public function test_URL_validation(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateURL("https://www.google.com");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_URL("https://www.google.com");
+                $this->assertEquals(true, $result);
         }
         
-        public function testURLValidation2(){
+        public function test_URL_validation_2(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateURL("google.com");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_URL("google.com");
+                $this->assertEquals(false, $result);
         }
         
-        public function testURLValidation3(){
+        public function test_URL_validation_3(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateURL("yahoo");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_URL("yahoo");
+                $this->assertEquals(false, $result);
         }
         
-        public function testURLValidation4(){
+        public function test_URL_validation_4(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateURL("www.google.com");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_URL("www.google.com");
+                $this->assertEquals(false, $result);
         }
         
-        public function testURLValidation5(){
+        public function test_URL_validation_5(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateURL("https://forums.phpfreaks.com/topic/284903-allow-only-letters-numbers-dash-underscore-or-space/");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_URL("https://forums.phpfreaks.com/topic/284903-allow-only-letters-numbers-dash-underscore-or-space/");
+                $this->assertEquals(true, $result);
         }
         
         /*
          * Title field validation tests
          */
-        public function testTitleValidation1(){
+        public function test_title_validation(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateTitle("i am a regular title");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_title("i am a regular title");
+                $this->assertEquals(true, $result);
         }
         
-        public function testTitleValidation2(){
+        public function test_title_validation_2(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateTitle("234987239484");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_title("234987239484");
+                $this->assertEquals(false, $result);
         }
         
-        public function testTitleValidation3(){
+        public function test_title_validation_3(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateTitle("This is a title!");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_title("This is a title!");
+                $this->assertEquals(true, $result);
         }
         
-        public function testTitleValidation4(){
+        public function test_title_validation_4(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateTitle("I contain letters and numb3r5!");
-                $this->assertEquals("Valid", $result);
+                $result = $tester->validate_title("I contain letters and numb3r5!");
+                $this->assertEquals(true, $result);
         }
         
-        public function testTitleValidation5(){
+        public function test_title_validation_5(){
                 $tester = new ValidationMethods();
-                $result = $tester->validateTitle("#$%^&*&^%$#%^&!");
-                $this->assertEquals("Invalid", $result);
+                $result = $tester->validate_title("#$%^&*&^%$#%^&!");
+                $this->assertEquals(false, $result);
+        }
+        
+        /*
+         * Description validation tests
+         */
+        public function test_desc_validation(){
+                $tester = new ValidationMethods();
+                $result = $tester->validate_description("#$%^&*&^%$#%^&!");
+                $this->assertEquals(false, $result);
+        }
+        
+        public function test_desc_validation_2(){
+                $tester = new ValidationMethods();
+                $result = $tester->validate_description("Normal description!");
+                $this->assertEquals(true, $result);
+        }
+        
+        public function test_desc_validation_3(){
+                $tester = new ValidationMethods();
+                $result = $tester->validate_description("            ");
+                $this->assertEquals(false, $result);
+        }
+        
+        /*
+         * Test contains letters
+         */
+        public function test_contains_letters(){
+                $tester = new ValidationMethods();
+                $result = $tester->contains_letters("            ");
+                $this->assertEquals(false, $result);
+        }
+        
+        public function test_contains_letters_2(){
+                $tester = new ValidationMethods();
+                $result = $tester->contains_letters("abcdefg234hijklmnop");
+                $this->assertEquals(true, $result);
+        }
+        
+        public function test_contains_letters_3(){
+                $tester = new ValidationMethods();
+                $result = $tester->contains_letters("#$%^&*(");
+                $this->assertEquals(false, $result);
+        }
+        
+        /*
+         * Test sanitize string
+         */
+        public function test_sanitize_string(){
+                $tester = new ValidationMethods();
+                $result = $tester->sanitize_string("      name      ");
+                $this->assertEquals("Name", $result);
+        }
+        
+        public function test_sanitize_string_2(){
+                $tester = new ValidationMethods();
+                $result = $tester->sanitize_string("   ");
+                $this->assertEquals("", $result);
+        }
+        
+        public function test_sanitize_string_3(){
+                $tester = new ValidationMethods();
+                $result = $tester->sanitize_string("\n\n\t\r YO");
+                $this->assertEquals("Yo", $result);
+        }
+        
+        public function test_sanitize_string_4(){
+                $tester = new ValidationMethods();
+                $result = $tester->sanitize_string("two   words");
+                $this->assertEquals("Two Words", $result);
+        }
+        
+        /*
+         * Test only letters
+         */
+        public function test_only_letters(){
+                $tester = new ValidationMethods();
+                $result = $tester->only_letters("asdfghjk");
+                $this->assertEquals(true, $result);
+        }
+        
+        public function test_only_letters_2(){
+                $tester = new ValidationMethods();
+                $result = $tester->only_letters("");
+                $this->assertEquals(false, $result);
+        }
+        
+        public function test_only_letters_3(){
+                $tester = new ValidationMethods();
+                $result = $tester->only_letters("\n\n\t\r YO");
+                $this->assertEquals(false, $result);
         }
 }
